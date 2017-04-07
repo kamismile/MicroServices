35fb81f9231a2c42fb010ec6507c6464a7cddfeb
saibal_rocks <saibal.ghosh.1980@gmail.com>
2017-04-07 11:14:31 +0800

    NA

---
+++ 3-CTS-MS-FLIGHT/src/main/java/com/cts/aggregate/FlightAggregate.java
@@ -0,0 +1,55 @@
+package com.cts.aggregate;
+
+import static org.axonframework.commandhandling.model.AggregateLifecycle.apply;
+
+import org.apache.log4j.Logger;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.commandhandling.model.AggregateIdentifier;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.spring.stereotype.Aggregate;
+
+import com.cts.bo.FlightReservationBO;
+import com.cts.command.FlightBookOrCancelCommand;
+import com.cts.event.FlightBookOrCancelEvent;
+import com.cts.event.FlightReservationDoneEvent;
+import com.cts.eventhandler.FlightBookOrCancelHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.NoArgsConstructor;
+
+@Aggregate
+@NoArgsConstructor
+public class FlightAggregate {
+	private static Logger log = Logger.getLogger(FlightAggregate.class);
+
+	@AggregateIdentifier
+	private String flightBookingReferenceNumber;
+
+	@CommandHandler
+	public FlightAggregate(FlightBookOrCancelCommand command) {		
+		//log.info("***************Wake Up***********************");
+		log.debug("-------------------------------You are all set Garfiedl! I will book your flight---------------------------------------");
+		apply(new FlightBookOrCancelEvent((FlightReservationBO)command.getReservationBO()));
+		apply(new FlightReservationDoneEvent((FlightReservationBO)command.getReservationBO()));// Notify
+																			// the
+																			// saga
+																			// to
+																			// move
+																			// ahead
+	}
+
+	@EventSourcingHandler
+	protected void on(FlightBookOrCancelEvent event) {
+		
+		this.flightBookingReferenceNumber = ((FlightReservationBO) event.getReservationBO())
+				.getFlightBookingReferenceNumber();
+		log.info("------FlightBookOrCancelEvent-----------");	
+
+	}
+
+	@EventSourcingHandler
+	protected void on(FlightReservationDoneEvent event) {		
+		this.flightBookingReferenceNumber = event.getReservationBO().getFlightBookingReferenceNumber();
+		log.info("------FlightReservationDoneEvent-----------");	}
+
+}
